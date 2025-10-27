1d6186abef4ee55e8ae62c8e0bbc5126697b8a76
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2025-10-27 12:45:23 +0000

    delete user api handler simplified

--- api/delete_user.go
+++ api/delete_user.go
@@ -5,38 +5,12 @@ import (
 
 	"github.com/Drolfothesgnir/shitposter/token"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5"
 )
 
-// TODO: refactor this
 func (service *Service) deleteUser(ctx *gin.Context) {
-	payload, ok := ctx.Get(authorizationPayloadKey)
-	if !ok {
-		ctx.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-	authPayload := payload.(*token.Payload)
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 
-	// find user to check if user can be deleted
-	user, err := service.store.GetUser(ctx, authPayload.UserID)
-	if err != nil {
-		// if there is no user with requested id abort
-		if err == pgx.ErrNoRows {
-			ctx.Status(http.StatusNoContent)
-			return
-		}
-
-		ctx.JSON(http.StatusInternalServerError, NewErrorResponse(err))
-		return
-	}
-
-	// check if user is deleted, abort if true
-	if user.IsDeleted {
-		ctx.Status(http.StatusNoContent)
-		return
-	}
-
-	err = service.store.SoftDeleteUserTx(ctx, authPayload.UserID)
+	err := service.store.SoftDeleteUserTx(ctx, authPayload.UserID)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, NewErrorResponse(err))
 		return

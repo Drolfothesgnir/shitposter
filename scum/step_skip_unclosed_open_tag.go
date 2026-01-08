822f7ac9d768bc2fc014a0813a2219512ce0f3ab
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2026-01-08 09:47:03 +0000

    removed StepProbeAndDecide entirely; conditional flow is now done with smaller in-step checks;

--- scum/step_skip_unclosed_open_tag.go
+++ scum/step_skip_unclosed_open_tag.go
@@ -1,8 +1,11 @@
 package scum
 
-// StepSkipOpenTag used when the opening Tag is malformed or unclosed, for the Tokenizer
-// to treat it as a plain text.
-func StepSkipOpenTag(ctx *ActionContext) bool {
+// StepSkipUnclosedOpenTag will do nothing if the [Tag] is closed.
+func StepSkipUnclosedOpenTag(ctx *ActionContext) bool {
+	if ctx.Bounds.Closed {
+		return false
+	}
+
 	// we explicitely tell that no Token will be returned
 	ctx.Token = Token{}
 
